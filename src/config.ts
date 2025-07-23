49a6c4b8bc78d784cb7396324f7f0869a662f863
Lucas Frontalini <frontalinilucas@gmail.com>
2025-07-23 12:18:45 -0300

    webhook url

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export default {
   maxListeners: 15,
   customUserDataDir: './userDataDir/',
   webhook: {
-    url: null,
+    url: 'https://communications-api.staging.mudafy.com/api/wpp-connect/webhook',
     autoDownload: true,
     uploadS3: false,
     readMessage: true,
